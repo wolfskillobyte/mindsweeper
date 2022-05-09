9156c68aa06d25fc2281a2fd7bd7fb0f9054dcd2
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2022-05-09 10:29:01 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let width = 10;
+  let bombAmount = 20;
+  let squares = [];
+
+  // create board
+  function createBoard() {
+    // get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill('bomb');
+    const emptyArray = Array(width * width - bombAmount).fill('valid');
+    // concatenate bombs array with empty array
+    const gameArray = emptyArray.concat(bombsArray);
+    // shuffle items in array using math random
+    const shuffArr = gameArray.sort(() => Math.random() - 0.5);
+
+    // creates grid of squares while i is less than 100 (width times width)
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', i);
+      square.classList.add(shuffArr[i]);
+      grid.appendChild(square);
+      squares.push(square);
+    }
+
+    // numbers to show # of bombs surrounding square that was clicked
+    for (let i = 0; i < squares.length; i++) {
+      // initiate total for counter
+      let total = 0;
+      const leftEdge = i % width === 0;
+      const rightEdge = i % width === width - 1;
+
+      if (squares[i].classList.contains('valid')) {
+        // checking left for bomb
+        if (i > 0 && !leftEdge && squares[i - 1].classList.contains('bomb'))
+          total++;
+        // checking right for bomb
+        if (
+          i > 9 &&
+          !rightEdge &&
+          squares[i + 1 - width].classList.contains('bomb')
+        )
+          total++;
+        
+        if (i > 10 && squares[i - width].classList.contains('bomb'))
+        
+        // add total of bombs surrounding square to a class attribute 
+        squares[i].setAttribute('data', total);
+        console.log(squares[i]);
+      }
+    }
+  }
+
+  createBoard();
+});
